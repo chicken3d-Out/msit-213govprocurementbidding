f9ab48b79df8e3c2376f4f516bb0691ca25da77f
chicken3d-Out <vincent.ontuca@evsu.edu.ph>
2026-05-30 15:44:45 +0800

    add cloudinary for media storage

--- settings/prod.py
+++ settings/prod.py
@@ -1,11 +1,21 @@
 from .base import *
 import os
 import dj_database_url
+import cloudinary
 
 DEBUG = False
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
+INSTALLED_APPS += ['cloudinary', 'cloudinary_storage']
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
 
 DATABASES = {
     'default': dj_database_url.config(
@@ -21,3 +31,5 @@ SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_BROWSER_XSS_FILTER = True
 X_FRAME_OPTIONS = 'DENY'
+
+
